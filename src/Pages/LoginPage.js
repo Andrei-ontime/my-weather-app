bfa558e02edee022b61a4801f70b0a1df0f59a02
Andrei <58655937+Andrei-ontime@users.noreply.github.com>
2021-12-12 22:05:17 +0300

    Validation form hooks

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,79 +1,36 @@
-import { useState, useEffect } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase-config";
-import { useNavigate } from "react-router";
-import { Link } from "react-router-dom";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+// import { useState, useEffect } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase-config';
+import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+import { useInput } from './hooks/useInput';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function LoginPage() {
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-  const [emailDirty, setEmailDirty] = useState(false);
-  const [passwordDirty, setPasswordDirty] = useState(false);
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [formValid, setFormValid] = useState(false);
+  const email = useInput('', {
+    isDirty: false,
+    isEmpty: true,
+    emailError: false,
+  });
+  const password = useInput('', { isEmpty: true, minLength: 7 });
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (emailError || passwordError) {
-      setFormValid(false);
-    } else {
-      setFormValid(true);
-    }
-  }, [emailError, passwordError]);
-
-  const emailHandler = (e) => {
-    setLoginEmail(e.target.value);
-    const re =
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (!re.test(String(e.target.value).toLocaleLowerCase())) {
-      setEmailError("Email is not correct");
-    } else {
-      setEmailError("");
-    }
-  };
-
-  const passwordHandler = (e) => {
-    setLoginPassword(e.target.value);
-    const re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    if (!re.test(loginPassword)) {
-      setPasswordError("password is not correct");
-    } else {
-      setPasswordError("");
-    }
-  };
-
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case "email":
-        setEmailDirty(true);
-        break;
-      case "password":
-        setPasswordDirty(true);
-        break;
-    }
-  };
 
   const login = async () => {
     try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
+      const user = await signInWithEmailAndPassword(auth, email, password);
 
       sessionStorage.setItem(
-        "Auth Token",
+        'Auth Token',
         user._tokenResponse.refreshToken,
 
-        console.log("Auth Token")
+        console.log('Auth Token')
       );
 
-      navigate("/");
+      navigate('/');
 
       console.log(user);
     } catch (error) {
@@ -83,58 +40,88 @@ function LoginPage() {
 
   return (
     <Box
-      component="form"
+      component='form'
       sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
-        "& > :not(style)": {
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
           m: 20,
           width: 200,
           height: 300,
         },
-      }}>
+      }}
+    >
       <Paper
         elevation={4}
         sx={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
           p: 10,
-        }}>
+        }}
+      >
         <h2>Log in</h2>
 
         <TextField
-          value={loginEmail}
-          onBlur={(e) => blurHandler(e)}
-          onChange={(e) => emailHandler(e)}
-          error={emailDirty}
-          helperText={emailError}
-          name="email"
-          label="Email"
-          variant="standard"
+          error={email.isDirty}
+          value={email.value}
+          onChange={(e) => email.onChange(e)}
+          onBlur={(e) => email.onBlur(e)}
+          name='email'
+          label='Email'
+          variant='standard'
         />
+        {email.isDirty && email.isEmpty && (
+          <Typography
+            color='red'
+            variant='caption'
+            display='block'
+            gutterBottom
+          >
+            email can not be empty
+          </Typography>
+        )}
+        {email.isDirty && email.emailError && (
+          <Typography
+            color='red'
+            variant='caption'
+            display='block'
+            gutterBottom
+          >
+            incorrect email
+          </Typography>
+        )}
         <TextField
-          value={loginPassword}
-          onBlur={(e) => blurHandler(e)}
-          onChange={(e) => passwordHandler(e)}
-          error={passwordDirty}
-          helperText={passwordError}
-          name="password"
-          label="Password"
-          variant="standard"
+          error={password.isDirty}
+          value={password.value}
+          onChange={(e) => password.onChange(e)}
+          onBlur={(e) => password.onBlur(e)}
+          name='password'
+          label='Password'
+          variant='standard'
         />
+        {password.isDirty && password.isEmpty && (
+          <Typography
+            color='red'
+            variant='caption'
+            display='block'
+            gutterBottom
+          >
+            password can not be empty
+          </Typography>
+        )}
         <Button
-          onClick={login}
-          disabled={!formValid}
-          variant="contained"
+          disabled={!email.inputValid}
+          variant='contained'
           sx={{
             m: 5,
-          }}>
+          }}
+        >
           Submit
         </Button>
-        <Link to="/authpage">create an account</Link>
+        <Link to='/authpage'>create an account</Link>
       </Paper>
     </Box>
   );
